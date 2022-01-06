c74b7a779a5427505647a25c52d52a02da5f4bbb
JPr0c <jules.procacci@hotmail.fr>
2022-01-06 16:50:02 +0100

    First commit
    06/01/2022

--- app/src/main/java/com/example/collector5g/CollectingService.java
+++ app/src/main/java/com/example/collector5g/CollectingService.java
@@ -1,7 +1,11 @@
 package com.example.collector5g;
 
+import static android.os.Build.MANUFACTURER;
+import static android.os.Build.MODEL;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -13,6 +17,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 
 import android.location.Location;
@@ -20,6 +27,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.BatteryManager;
 
+import android.os.Build;
 import android.os.IBinder;
 
 import android.util.Log;
@@ -28,10 +36,23 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.concurrent.Executor;
+
 
 public class CollectingService extends Service {
     public CollectingService() {
     }
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SensorManager mySensorManager;
+    private Sensor myAccelerometer;
+    private Sensor myGyroscope;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -42,32 +63,92 @@ public class CollectingService extends Service {
 
     @Override
     public void onCreate() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        myAccelerometer = mySensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);  // excludes gravity, good for motion detection
+        myGyroscope = mySensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         super.onCreate();
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("STATUS", "service started");
-        Log.i("BATTERY LEVEL", "" + getBatteryLevel() + "%");
-        Log.i("Getting location", "processing");
-
+        getLocation();
+        getMobility();
+        getBatteryLevel();
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public void getMobility(){
-        SensorManager sensorManager;
-        Sensor msensor;
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        msensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
-    public int getBatteryLevel() {
+
+    public void getMobility() {
+
+            mySensorManager.registerListener(new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    if (event != null) {
+                        MainActivity.accel_x.setText(String.format("%.2f", event.values[0]));
+                        MainActivity.accel_y.setText(String.format("%.2f", event.values[1]));
+                        MainActivity.accel_z.setText(String.format("%.2f", event.values[2]));
+                    }
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                }
+            }, myAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+
+
+/*
+        mySensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event != null) {
+                    MainActivity.gyros_x.setText(String.format("%.2f", event.values[0]));
+                    MainActivity.gyros_y.setText(String.format("%.2f", event.values[1]));
+                    MainActivity.gyros_z.setText(String.format("%.2f", event.values[2]));
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        }, myGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+ */
+
+    }
+
+
+    private void getLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+
+                        MainActivity.latitude.setText("Latitude : " + location.getLatitude() + " °N");
+                        MainActivity.longitude.setText("Longitude : " + location.getLongitude() + " °E");
+                        MainActivity.altitude.setText("Altitude : " + (int) (Math.round(location.getAltitude() * 100)) / 100.0 + " meters");
+
+                    }
+                });
+    }
+
+    public void getBatteryLevel() {
         // get battery level
         BatteryManager bm = (BatteryManager)getSystemService(BATTERY_SERVICE);
         int batteryPercentage = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-        return batteryPercentage;
+        //Log.i("BATTERY LEVEL", "" + batteryPercentage + "%");
+        MainActivity.battery.setText("BATTERY LEVEL: " + batteryPercentage + "%");
     }
-
 }
--- app/src/main/java/com/example/collector5g/MainActivity.java
+++ app/src/main/java/com/example/collector5g/MainActivity.java
@@ -1,20 +1,68 @@
 package com.example.collector5g;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.os.Build.MANUFACTURER;
+import static android.os.Build.MODEL;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    //device info
+    TextView dn;
+    TextView dm;
+    TextView dv;
+
+    //location
+    static TextView Location;
+    static TextView latitude;
+    static TextView longitude;
+    static TextView altitude;
+
+
+    //mobility accelerometer
+    static TextView accel_data;
+    static TextView accel_x;
+    static TextView accel_y;
+    static TextView accel_z;
+
+    //mobility gyroscope
+    static TextView gyros_data;
+    static TextView gyros_x;
+    static TextView gyros_y;
+    static TextView gyros_z;
+
+    //battery display
+    static TextView battery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponent();
+        getDeviceÌnfo();
 
         Button startButton = findViewById(R.id.startButton);
         Button stopButton = findViewById(R.id.stopButton);
@@ -26,10 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 serviceIntent.setAction("start collecting service");
                 startService(serviceIntent);
 
-                //Intent locationIntent = new Intent(getApplicationContext(), CollectingService.class);
-                //locationIntent.setAction("start location service");
-                //startService(locationIntent);
-            }
+                }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
@@ -38,27 +83,31 @@ public class MainActivity extends AppCompatActivity {
                 Intent serviceIntent = new Intent(getApplicationContext(), CollectingService.class);
                 serviceIntent.setAction("stop collecting service");
                 stopService(serviceIntent);
-
-                //Intent locationIntent = new Intent(getApplicationContext(), CollectingService.class);
-                //locationIntent.setAction("stop location service");
-                //stopService(locationIntent);
             }
         });
-
-
-        /*Switch switch1 = (Switch) findViewById(R.id.switch1);
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // start our service
-                Intent serviceIntent = new Intent(getApplicationContext(), CollectingService.class);
-                serviceIntent.setAction("start collecting service");
-                startService(serviceIntent);
-
-                Intent locationIntent = new Intent(getApplicationContext(), CollectingService.class);
-                locationIntent.setAction("start location service");
-                startService(locationIntent);
-            }
-        }) ;*/
     }
+
+    private void initComponent() {
+        accel_data = findViewById(R.id.accel_data);
+        accel_x = findViewById(R.id.accel_x);
+        accel_y = findViewById(R.id.accel_y);
+        accel_z = findViewById(R.id.accel_z);
+
+        latitude =  findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+        altitude = findViewById(R.id.altitude);
+
+        dn = (TextView) findViewById(R.id.dn);
+        dm = (TextView) findViewById(R.id.dm);
+        dv = (TextView) findViewById(R.id.dv);
+
+        battery = (TextView) findViewById(R.id.battery);
+    }
+
+    private void getDeviceÌnfo() {
+        dn.setText(MANUFACTURER);
+        dm.setText(MODEL);
+        dv.setText("Android " + Build.VERSION.RELEASE);
+    }
+
 }
